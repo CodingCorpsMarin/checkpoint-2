6492c4b84a91ec269ddc394cf28f6972f2b1233a
CarlosM90 <mendoza1990carlos@gmail.com>
2017-09-15 15:02:42 -0700

    fuck soda

--- soda_machine.rb
+++ soda_machine.rb
@@ -4,15 +4,27 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @sold_soda = ""
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+     soda = find_soda(soda_brand)
+     if soda != nil
+       @sodas.delete(soda)
+       @cash = @cash + soda.price
+       return soda
+     end
+     nil
   end
-
+ 
 end
+
+
