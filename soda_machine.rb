c7e6fd140cf0cfc3f8c6edfc0d64137e8d67b02d
yeswecode <yeswecode@yeswecodes-MacBook-Pro.local>
2017-09-15 15:00:44 -0700

    :(

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,5 @@
 class SodaMachine
+
   attr_reader :sodas, :cash
 
   def initialize(args = {})
@@ -7,12 +8,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|x| x.brand == soda_brand} 
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand) == false
+       nil
+    else
+      @sold_soda = soda_brand
+      @sold_soda
+      @cash << @sold_soda.price
+      @sodas.delete(@sold_soda)
+    end
   end
 
 end
